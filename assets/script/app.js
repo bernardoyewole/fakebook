282492a3fdff1035a0bb607eae955215bb4acaab
Bernard <oyebenard@gmail.com>
2023-11-29 14:41:32 -0600

    Static page complete

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,26 +4,26 @@ import { onEvent, select, selectAll, create, print } from "./utils.js";
 
 import { Subscriber } from "./Subscriber.js";
 
-// let groupsArr = [
-//     'Ninja Penguin',
-//     'Sloth Fanatics', 
-//     'Crazy Hat Club', 
-//     'Toast Lovers'
-// ];
+let groupsArr = [
+    'Ninja Penguin',
+    'Sloth Fanatics', 
+    'Crazy Hat Club', 
+    'Toast Lovers'
+];
 
-// let pagesArr = [
-//     'Haiku Helpline',
-//     'Galactic Foodies',
-//     'Punderful Puppies',
-//     'Zen Garden of Memes'
-// ];
+let pagesArr = [
+    'Haiku Helpline',
+    'Galactic Foodies',
+    'Punderful Puppies',
+    'Zen Garden of Memes'
+];
 
-// const newSubscriber = new Subscriber(
-//     '3458fff', 'Bernard Oyewole', 'bernard123', 'bernard@gmail.com',
-//     groupsArr, pagesArr, true
-// );
+const newSubscriber = new Subscriber(
+    '3458fff', 'Bernard Oyewole', 'bernard123', 'bernard@gmail.com',
+    groupsArr, pagesArr, true
+);
 
-// console.log(newSubscriber.getInfo())
+console.log(newSubscriber.getInfo())
 
 // Modal
 const dialog = select('.dialog');
@@ -45,6 +45,62 @@ window.addEventListener('click', (event) => {
 });
 
 // Post
+const post = select('.post');
+const text = select('textarea');
+const fileInput = select('#file-input');
+const fakebook = select('.fakebook');
 
+// print(fileInput.files)
+// print(text.value)
+
+function getText() {
+    return text.value.trim();
+}
+
+function getImage() {
+    let file = fileInput.files[0];
+    if (file.type.startsWith('image/')) {
+        let img = create('img');
+        img.src = URL.createObjectURL(file);
+        return img;
+    }
+}
+
+function postHeaderContent() {
+    let userIcon = create('img');
+    userIcon.src = '../img/contacts-24.png';
+    let name = create('p');
+    name.innerText = newSubscriber.name;
+    let date = create('p');
+    date.innerText = new Date().toLocaleDateString()
+    return [userIcon, name, date];
+}
+
+function createHeader() {
+    let header = create('div');
+    header.classList.add('flex');
+    let content = postHeaderContent();
+    console.log(content)
+    content.forEach(arg => {
+        header.appendChild(arg);
+    })
+    return header;
+}
+
+function createPost() {
+    let header = createHeader();
+    let postContainer = create('div');
+    let post = create('p');
+    let img = getImage();
+    post.innerText = getText();
+    postContainer.appendChild(header);
+    postContainer.appendChild(post);
+    postContainer.appendChild(img);
+    fakebook.appendChild(postContainer);
+}
+
+onEvent('click', post, () => {
+    createPost();
+})
 
 
